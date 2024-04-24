fa8fece1d0154a8c09521e3a7e96f76cdf987538
Joey <joeylim0150@gmail.com>
2024-04-24 13:39:27 +0800

    ...

--- urls.py
+++ urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for deliver project.
+"""deliver URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,13 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-from customer.views import Index, About, Order
+from customer.views import Index, About
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', Index.as_view(), name="index"),
-    path('about/', About.as_view(), name="about"),
-    path('order/', Order.as_view(), name="order")
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', Index.as_view(), name='index'),
+    path('about/', About.as_view(), name='about'),
+]
