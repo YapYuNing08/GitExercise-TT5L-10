f2799d29d68c2769327a9f31b8ab3d2fd5612de3
waiyan2005 <167391573+waiyan2005@users.noreply.github.com>
2024-05-31 21:25:16 +0800

    Update status

--- deliver/restaurant/urls.py
+++ deliver/restaurant/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('restaurant/index/', Index.as_view(), name='restaurant_index'),
     path('restaurant/reservation_details/<int:pk>/', ReservationDetails.as_view(), name='reservation_details'),
     path('mark_as_served/<int:order_id>/', MarkAsServed.as_view(), name='mark_as_served'),
+    path('restaurant/update_food_status/<int:order_id>/',views.update_food_status, name='update_food_status'),
     ]
