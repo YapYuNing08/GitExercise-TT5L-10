ecaeeef40a30896c3af8920169603a781032effb
joeylim22 <166042526+joeylim22@users.noreply.github.com>
2024-06-05 22:24:44 +0800

    Update admin.py

--- deliver/customer/admin.py
+++ deliver/customer/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from . models import MenuItem, Category, OrderModel, Product, Cart, Customer, ReservationModel, OrderPlaced, RedemptionOption, RedeemedItem, Review
+from . models import MenuItem, Category, OrderModel, Product, Cart, Customer, ReservationModel, OrderPlaced, CustomizationOption, CustomizationChoice, RedemptionOption, RedeemedItem, Review
 
 admin.site.register(MenuItem)
 admin.site.register(Category)
 admin.site.register(OrderModel)
 admin.site.register(ReservationModel)
+admin.site.register(CustomizationOption)
+admin.site.register(CustomizationChoice)
 
 @admin.register(Product)
 class ProductModelAdmin(admin.ModelAdmin):
@@ -21,6 +23,12 @@ class CustomerModelAdmin(admin.ModelAdmin):
 @admin.register(OrderPlaced)
 class OrderPlacedModelAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'product', 'quantity','ordered_date']
+
+class CustomizationChoiceInline(admin.TabularInline):
+    model = CustomizationChoice
+
+class CustomizationOptionAdmin(admin.ModelAdmin):
+    inlines = [CustomizationChoiceInline]
 
 @admin.register(RedemptionOption)
 class RedemptionOptionModelAdmin(admin.ModelAdmin):
