89ead4e902fb222e0749bea117fe74e6ded5e0ba
Yap Yu Ning <yapyuning0824@gmail.com>
2024-06-12 17:43:30 +0800

    Update settings to use python-decouple

--- deliver/deliver/settings.py
+++ deliver/deliver/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 import django_heroku
 import dj_database_url
 
@@ -97,13 +98,16 @@ WSGI_APPLICATION = 'deliver.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
