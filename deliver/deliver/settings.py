ed33603f5164df9d6924ed8b83b31c7dff29f580
waiyan2005 <cheongwaiyan2005@email.com>
2024-06-13 10:37:47 +0800

    .

--- deliver/deliver/settings.py
+++ deliver/deliver/settings.py
@@ -98,16 +98,14 @@ WSGI_APPLICATION = 'deliver.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
