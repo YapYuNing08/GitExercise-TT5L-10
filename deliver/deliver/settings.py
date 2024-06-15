0a4f31c0b9309b6aa3a129c36e058851e73e46a0
waiyan2005 <cheongwaiyan2005@email.com>
2024-06-15 17:00:42 +0800

    update

--- deliver/deliver/settings.py
+++ deliver/deliver/settings.py
@@ -105,6 +105,11 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(default='postgres://localhost')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
